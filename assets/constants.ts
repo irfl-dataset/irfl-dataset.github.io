f7987be7b777b252543c49b47aaf0d567b7b2195
Ron <nrdronys@gmail.com>
2023-03-16 00:04:34 +0200

    test

---
+++ assets/constants.ts
@@ -0,0 +1,19 @@
+export const QUERIES_BLACKLIST = [
+    'to delay sexual ejaculation',
+    'to give somebody oral sex',
+    'to achieve erection for sexual intercourse',
+    'associated with the high church (the church of england and other anglican traditions, in reference to their \"high\" political position in england and \"dry\"/austere mode of worship), as opposed to the \"low and slow\" evangelical party and the \"broad and shallow\" broad church',
+    'to reveal that one has fabricated or deliberately misrepresented one\'s own behavior, situation, or identity for an ulterior motive',
+    'in the game of cribbage, to reach the point at which one is safe from being lurched',
+    'a discreet, unassuming reference to freemasonry',
+    'dirty',
+    'to overcome obstacles and make a beginning',
+    'blunt',
+    'moving',
+    'to note',
+    'synonym of out of one\'s own pocket',
+    'not available',
+    'of or pertaining to the spending of cash rather than using credit',
+    'to disillusion',
+    ''
+]
